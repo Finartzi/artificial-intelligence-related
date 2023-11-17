7f48eb8e325e39f988159f78feca78293d940c83
Arto Päivinen <arto.paeivinen@gmail.com>
2023-11-17 23:42:12 +0200

    Creating text chunks

--- postgres-pgvector-extension/pgvector-demo.py
+++ postgres-pgvector-extension/pgvector-demo.py
@@ -7,5 +7,10 @@ load_dotenv()
 
 loader = TextLoader('./resources/state_of_the_union.txt', encoding='utf-8')
 documents = loader.load()
-print(documents)
+#print(documents)
 print(len(documents))
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=80)
+texts = text_splitter.split_documents(documents)
+#print(texts[0])
+print(len(texts))
